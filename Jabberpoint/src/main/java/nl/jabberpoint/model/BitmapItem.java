ba6a5a56cdc46f17d89d7515bfe15e194834f12e
Cedric Smit <cedsmit@gmail.com>
2026-03-26 10:27:05 +0100

    fixed duplicated methods

--- Jabberpoint/src/main/java/nl/jabberpoint/model/BitmapItem.java
+++ Jabberpoint/src/main/java/nl/jabberpoint/model/BitmapItem.java
@@ -76,22 +76,6 @@ public class BitmapItem extends SlideItem {
 		return imageName;
 	}
 
-// geef de bounding box van de afbeelding
-	public Rectangle getBoundingBox(Graphics g, ImageObserver observer, float scale, Style myStyle) {
-		return new Rectangle((int) (myStyle.indent * scale), 0,
-				(int) (bufferedImage.getWidth(observer) * scale),
-				((int) (myStyle.leading * scale)) +
-				(int) (bufferedImage.getHeight(observer) * scale));
-	}
-
-// teken de afbeelding
-	public void draw(int x, int y, float scale, Graphics g, Style myStyle, ImageObserver observer) {
-		int width = x + (int) (myStyle.indent * scale);
-		int height = y + (int) (myStyle.leading * scale);
-		g.drawImage(bufferedImage, width, height,(int) (bufferedImage.getWidth(observer)*scale),
-                (int) (bufferedImage.getHeight(observer)*scale), observer);
-	}
-
 	public String toString() {
 		return "BitmapItem[" + getLevel() + "," + imageName + "]";
 	}
